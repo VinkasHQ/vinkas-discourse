8ca78c834e60b0de4b6a56aef51e0dbf908f3c8a
Vinoth Kannan <vinothkannan@vinkas.com>
2017-09-15 16:29:41 +0530

    FEATURE: Branding header settings added

---
+++ plugin.rb
@@ -0,0 +1,30 @@
+# name: vinkas
+# about: Plugin for Vinkas
+# version: 0.0.1
+# authors: Vinoth Kannan (@vinothkannans)
+# url: https://github.com/vinkashq/vinkas-discourse
+
+after_initialize do
+  module ::Vinkas
+    PLUGIN_NAME = "vinkas".freeze
+
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscourseAutobot
+    end
+
+    class Store
+      def self.set(key, value)
+        ::PluginStore.set(PLUGIN_NAME, key, value)
+      end
+
+      def self.get(key)
+        ::PluginStore.get(PLUGIN_NAME, key)
+      end
+
+      def self.remove(key)
+        ::PluginStore.remove(PLUGIN_NAME, key)
+      end
+    end
+  end
+end
